0c916d5710767067a1b30f69090740508f0d438a
Bobby Nannier <bobby@nannier.com>
2017-09-22 16:19:11 -0400

    Api use

--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
 import ListContactsStateless from './ListContactsStateless'
+import * as ContactsAPI from './utils/ContactsAPI'
+
 
 class App extends Component {
     state = {
-        contacts: [
-            {
-                "id": "ryan",
-                "name": "Ryan Florence",
-                "email": "ryan@reacttraining.com",
-                "avatarURL": "http://localhost:5001/ryan.jpg"
-            },
-            {
-                "id": "michael",
-                "name": "Michael Jackson",
-                "email": "michael@reacttraining.com",
-                "avatarURL": "http://localhost:5001/michael.jpg"
-            },
-            {
-                "id": "tyler",
-                "name": "Tyler McGinnis",
-                "email": "tyler@reacttraining.com",
-                "avatarURL": "http://localhost:5001/tyler.jpg"
-            }
-        ]
+        contacts: []
     }
-
+    componentDidMount() {
+        ContactsAPI.getAll().then((contacts) => {
+            this.setState({ contacts })
+        })
+    }
     removeContact = (contact) => {
         this.setState((state) => ({
             contacts: state.contacts.filter((c) => c.id !== contact.id)
         }))
-    }
 
+        ContactsAPI.remove(contact)
+    }
     render() {
         return (
             <div>
+                <br /><br /><br />
                 <div>3.3: Stateless functional component</div>
                 <ListContactsStateless onDeleteContact={this.removeContact} contacts={this.state.contacts} />
-                <br />
                 <div>3.4: Stateful component</div>
                 <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
                 <br />
